9a0930776ebd8f6850298d8711803ef00c8f99fc
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-07-04 21:40:17 +0400

    New user validation are added to user validation class

--- AuthenticationWithClie/AuthenticationWithClie/ApplicationLogic/Validations/UserValidation.cs
+++ AuthenticationWithClie/AuthenticationWithClie/ApplicationLogic/Validations/UserValidation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AuthenticationWithClie.Database.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace AuthenticationWithClie.ApplicationLogic.Validations
 {
-    public class UserValidation
+    public static class UserValidation
     {
         public static bool IsValidFirstName(string firstName)
         {
@@ -27,20 +28,45 @@ namespace AuthenticationWithClie.ApplicationLogic.Validations
                 return true;
             }
 
-            Console.WriteLine("First name's length should be greater than 3 and less than 30");
+            Console.WriteLine("Last name's length should be greater than 5 and less than 20");
 
             return false;
         }
 
         public static bool IsValidEmail(string email)
         {
-            return true;
+            if (Validation.Contains(email, '@'))
+            {
+                return true;
+            }
+
+            Console.WriteLine("Email should contain @ characher.");
+
+            return false;
         }
 
-
-        public static bool IsValidPassword(string password, string confirtPassword)
+        public static bool IsValidPassword(string password, string confirmPassword)
         {
-            return true;
+            if (password == confirmPassword)
+            {
+                return true;
+            }
+
+            Console.WriteLine("Password is not match");
+
+            return false;
+        }
+
+        public static bool IsUserExist(string email)
+        {
+            if (UserRepository.IsUserExistsByEmail(email))
+            {
+                Console.WriteLine("User already exists");
+
+                return true;
+            }
+
+            return false;
         }
     }
 }
