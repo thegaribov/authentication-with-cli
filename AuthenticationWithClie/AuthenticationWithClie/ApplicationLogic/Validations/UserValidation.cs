56359050e0588d3e29b06937e23731bde7141483
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-07-18 20:22:20 +0400

    Regex validation for first name, last name and email completed

--- AuthenticationWithClie/AuthenticationWithClie/ApplicationLogic/Validations/UserValidation.cs
+++ AuthenticationWithClie/AuthenticationWithClie/ApplicationLogic/Validations/UserValidation.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AuthenticationWithClie.ApplicationLogic.Validations
@@ -11,42 +12,62 @@ namespace AuthenticationWithClie.ApplicationLogic.Validations
     {
         public static bool IsValidFirstName(string firstName)
         {
-            if (Validation.IsLengthBetween(firstName, 3, 30))
+            Regex regex = new Regex(@"^(?=[A-Z]{1})([A-Za-z]{3,30})$");
+
+            if (regex.IsMatch(firstName))
             {
                 return true;
             }
 
-            Console.WriteLine("First name's length should be greater than 3 and less than 30");
+            Console.WriteLine("Daxil etdiyiniz ad yanlışdır, adın yalnız hərflərdən ibarət olduğuna, ilk hərfin böyük olduğuna və uzunluğunun 3 dən böyük, 30 - dan kiçik olduğuna əmin olun.");
 
             return false;
         }
 
         public static bool IsValidLastName(string lastName)
         {
-            if (Validation.IsLengthBetween(lastName, 5, 20))
+            Regex regex = new Regex(@"^(?=[A-Z]{1})([A-Za-z]{3,30})$");
+
+            if (regex.IsMatch(lastName))
             {
                 return true;
             }
 
-            Console.WriteLine("Last name's length should be greater than 5 and less than 20");
+            Console.WriteLine("Daxil etdiyiniz soyad yanlışdır, adın yalnız hərflərdən ibarət olduğuna, ilk hərfin böyük olduğuna və uzunluğunun 3 dən böyük, 30 - dan kiçik olduğuna əmin olun.");
 
             return false;
         }
 
         public static bool IsValidEmail(string email)
         {
-            if (Validation.Contains(email, '@'))
+            Regex regex = new Regex(@"^[A-Za-z0-9]{10,30}@code\.edu\.az$");
+
+            if (regex.IsMatch(email))
             {
                 return true;
             }
 
-            Console.WriteLine("Email should contain @ characher.");
+            Console.WriteLine("Daxil etdiyiniz email yanlis formatdadir");
 
             return false;
         }
 
-        public static bool IsValidPassword(string password, string confirmPassword)
+        public static bool IsPasswordsMatch(string password, string confirmPassword)
         {
+
+            if (password == confirmPassword)
+            {
+                return true;
+            }
+
+            Console.WriteLine("Password is not match");
+
+            return false;
+        }
+
+        public static bool IsValidPassword(string password)
+        {
+
             if (password == confirmPassword)
             {
                 return true;
